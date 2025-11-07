e8b25d27c100823ba5a63ca1f5d3b8f9969007c4
Lois Joseph <loisjoseph.it@gmail.com>
2025-11-07 12:59:44 -0500

    Update and rename WN11-CC-000110 to WN11-CC-000110.ps1

---
+++ STIGS/WN11-CC-000110.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures correct accounts are used on the system for privileged tasks to help mitigate credential theft..
+
+.NOTES
+    Author          : Lois Joseph
+    LinkedIn        : linkedin.com/in/lois-joseph/
+    GitHub          : github.com/LoisJoseph0
+    Date Created    : 2025-09-11
+    Last Modified   : 2025-09-11
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000255
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-SO-000255).ps1 
+#>
