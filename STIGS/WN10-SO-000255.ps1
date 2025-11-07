2ba458e83d6eccb8c26c7645f166d991cb3a7731
Lois Joseph <loisjoseph.it@gmail.com>
2025-11-07 11:19:04 -0500

    Update WN10-SO-000255.ps1

--- STIGS/WN10-SO-000255.ps1
+++ STIGS/WN10-SO-000255.ps1
@@ -22,7 +22,7 @@
 .USAGE
     Put any usage instructions here.
     Example syntax:
-    PS C:\> .\STIG-ID-WN10-SO-000255.ps1 
+    PS C:\> .\__remediation_template(STIG-ID-WN10-SO-000255).ps1 
 #>
 
 # Self-elevate if not already admin
