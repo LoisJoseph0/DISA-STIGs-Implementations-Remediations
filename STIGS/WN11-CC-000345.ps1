4081a259fb40a14fa59b6d8a7ca7d8d53607da80
Lois Joseph <loisjoseph.it@gmail.com>
2025-11-08 14:19:50 -0500

    Update WN11-CC-000345.ps1

--- STIGS/WN11-CC-000345.ps1
+++ STIGS/WN11-CC-000345.ps1
@@ -1,1 +1,26 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that
 
+.NOTES
+    Author          : Lois Joseph
+    LinkedIn        : linkedin.com/in/lois-joseph/
+    GitHub          : github.com/LoisJoseph0
+    Date Created    : 2025-11-08
+    Last Modified   : 2025-11-08
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000345
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN11-CC-000345).ps1 
+#>
